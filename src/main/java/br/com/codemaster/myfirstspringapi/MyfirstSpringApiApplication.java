2450389677d5270ae47522d6d5b785f04ac5c6c1
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-09-11 17:42:28 -0300

    initial commit

---
+++ src/main/java/br/com/codemaster/myfirstspringapi/MyfirstSpringApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.codemaster.myfirstspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstSpringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/codemaster/myfirstspringapi/MyfirstSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codemaster.myfirstspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfirstSpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
