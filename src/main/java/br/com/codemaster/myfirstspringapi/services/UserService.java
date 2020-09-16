4010acc00b83a35a40cd391c98135fb1897817dc
Armando Tadeu Del Col <atdc.codemaster@gmail.com>
2020-09-16 16:05:46 -0300

    Excpetion Handler findById

--- src/main/java/br/com/codemaster/myfirstspringapi/services/UserService.java
+++ src/main/java/br/com/codemaster/myfirstspringapi/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.codemaster.myfirstspringapi.entities.User;
 import br.com.codemaster.myfirstspringapi.repositories.UserRepository;
+import br.com.codemaster.myfirstspringapi.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
